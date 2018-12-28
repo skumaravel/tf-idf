5913ab8bf2f35b76b80b0c966f444daa26c8aeff
skumaravel <skumara@g.clemson.edu>
2018-12-28 15:13:16 +0530

    Initial Commit

---
+++ tf_idf.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 28 13:00:09 2018
+
+@author: sujeeth.kumaravel
+@reference: www.tfidf.com
+
+This program implements tf-idf method.
+"""
+
+import math
+import string 
+
+def tf_idf(corpus, document, term):
+    
+    document = document.lower()
+    
+    exclude = set(string.punctuation)
+    document = ''.join(ch for ch in document if ch not in exclude)
+    
+    doc_term_list = document.split()
+    doc_term_count = len(doc_term_list)
+    
+    term_count_in_document = 0
+    for t in doc_term_list:
+        if t == term:
+            term_count_in_document += 1
+       
+    tf = term_count_in_document/doc_term_count
+    
+    doc_count = len(corpus)
+    
+    num_docs_with_term = 0
+    for doc in corpus:
+        doc = doc.lower()
+        doc = ''.join(ch for ch in doc if ch not in exclude)
+        d_words = doc.lower().split()
+        
+        if term in d_words:
+            num_docs_with_term += 1
+            
+    temp_idf = doc_count/num_docs_with_term
+    idf = math.log(temp_idf)
+    
+    return tf*idf
---
+++ tf_idf_test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 28 13:09:53 2018
+
+@author: sujeeth.kumaravel
+"""
+
+import string
+
+corpus = []
+doc1 = 'do not mess with me'
+doc2 = 'if not, that is going to be a damaging experience'
+doc3 = 'i will even kill you'
+doc4 = 'tomorrow is saturday'
+doc5 = 'cooking is done is kitchen. pooping is done in restroom'
+
+corpus.append(doc1)
+corpus.append(doc2)
+corpus.append(doc3)
+corpus.append(doc4)
+corpus.append(doc5)
+
+doc = 'tomorrow is saturday'
+
+word= 'tomorrow'
+
+a = tf_idf(corpus, doc, word)
+print(a)
